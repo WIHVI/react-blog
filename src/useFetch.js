382f0d09a5e9d340f20191074bb74c60a27a9d1c
WIHVI <112943833+WIHVI@users.noreply.github.com>
2022-11-20 12:26:12 +0200

    useFetch setTimeout removed and media query added

--- src/useFetch.js
+++ src/useFetch.js
@@ -8,28 +8,23 @@ const useFetch = (url) => {
   useEffect(() => {
     const abortCont = new AbortController();
 
-    setTimeout(() => {
-      fetch(url, { signal: abortCont.signal })
-        .then((res) => {
-          if (!res.ok) {
-            throw Error('could not fetch the data for that resource');
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setIsPending(false);
-          setData(data);
-          setError(null);
-        })
-        .catch((err) => {
-          if (err.name === 'AbortError') {
-            //console.log('fetch aborted');
-          } else {
-            setIsPending(false);
-            setError(err.message);
-          }
-        });
-    }, 300);
+    fetch(url, { signal: abortCont.signal })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error('could not fetch the data for that resource');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setIsPending(false);
+        setData(data);
+        setError(null);
+      })
+      .catch((err) => {
+        if (err.name === 'AbortError') return;
+        setIsPending(false);
+        setError(err.message);
+      });
 
     return () => abortCont.abort();
   }, [url]);
